0aea5b34b5a64d18ae6b691f76cbe741f51dc033
JarodMedina55 <54414612+MrLegolover55@users.noreply.github.com>
2019-09-18 11:13:58 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,34 @@
+//Java class ForLoops.java
+
+/**
+ * Skeleton code for the for loops practice day.
+ * 
+ * @author Roy Cancellor
+ * version 2/26/2019
+ */
+public class ForLoops
+{
+    public static void main ( String args[] ) {
+        //variable delarations
+        //int i, cube; int a;
+        //for loop to perform the task
+        //System.out.println();
+        //System.out.println("i\tcube i");
+        //for(i = 1; i <= 10; i = i + 1) {
+            //cube = i * i * i;
+            //System.out.println(i +"\t" + cube);
+        //}
+        //System.out.println();
+        //System.out.println("a");
+        //for(a = 99; a >= 1; a = a - 2) {
+            //System.out.println(a);
+        //}
+        String myStr;
+        int n;
+        n = myStr.length();
+        myStr.charAt (19);
+        System.out.println(myStr);
+        
+    }
+      //end main
+}  //end ForLoops class 
